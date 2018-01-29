456b9cf8b934d5c9887037c44f84d3e8b8216894
Kevin Shi <kshi@berkeley.edu>
2018-01-29 00:31:34 -0800

    new security features, implemented ordering of applications, fixed ambiguous reviewer names

--- writtenapp/reader.py
+++ writtenapp/reader.py
@@ -1,13 +1,12 @@
 import csv
 import random
 import airtable
+import collections
+from secrets import *
 
-# Airtable keys
-API_KEY = 'keyd6qkfF0Q6csT9G'
-APPLICATION_BASE_ID = 'apppVfnNT3wEXplHg'
-DECISION_BASE_ID = 'appo9HwPc9Oy8IZcG'
-
-TOTAL_YES = 50
+TOTAL_YES = 40
+DECISION_TABLE_NAME = 'Decisions'
+APPLICATION_TABLE_NAME = 'All%20Applications'
 
 # Colors for printing
 class color:
@@ -22,7 +21,56 @@ class color:
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
-reviewer_name = str(input('What is your name? '))
+# The reason we need this is because the shitty airtable API doesn't keep the questions in order...
+def reorder_dict(ordered_dict):
+	new_ordered_dict = collections.OrderedDict()
+	fields = ['Name', \
+				'Email', \
+				'Phone Number', \
+				'Year', \
+				'Why do you want to join ANova? How do you personally resonate with ANova\'s mission statement? (minimum 300 words)', \
+				'Tell us about your most memorable teaching or mentorship experience. (max 150 words)', \
+				'What does addressing systemic issues and inequality in education look like to you? (max 150 words)', \
+				'Which committee is your first choice?', \
+				'Explain why the committee above is your first choice. (minimum 100 words)', \
+				'Which committee is your second choice?', \
+				'Explain why the committee above is your second choice. (minimum 100 words)', \
+				'Which of these classes have you completed and passed?', \
+				'Which of these languages do you know?', \
+				'Please indicate ALL your availabilities (allocate 45 minutes before and after the time slots for travel).', \
+				'Can you attend orientation and retreat on 2/9 - 2/11?', \
+				'Do you have any other comments or questions for us?', \
+				'Time Submitted'
+			]
+
+	for field in fields:
+		if field in ordered_dict.keys():
+			new_ordered_dict[field] = ordered_dict[field]
+
+	return new_ordered_dict\
+
+# This is to ensure everyone uses the same name to review if they close the app based on the issues we ran into last semester
+reviewer_names = ['Aditya', \
+				'Andrew', \
+				'Christina', \
+				'David', \
+				'Emilie', \
+				'Franco', \
+				'Gillian', \
+				'Helen', \
+				'Jesse', \
+				'Joseph', \
+				'Katie', \
+				'Kevin', \
+				'Maggie', \
+				'William', \
+				'Yitian'
+			]
+
+reviewer_name = str(input('What is your name? ')).title()
+if reviewer_name not in reviewer_names:
+	print('The inputted name is not valid. Please provide your full first name. (i.e. Kevin)')
+	quit()
 print('\n')
 
 # Creates airtable object for use with API
@@ -30,10 +78,11 @@ application_at = airtable.Airtable(APPLICATION_BASE_ID, API_KEY)
 decision_at = airtable.Airtable(DECISION_BASE_ID, API_KEY)
 
 # Adding reviewed applicants to list so we don't have to re-review them
-decisions_list = decision_at.get('Decisions')
+# Get all applications that have a decision
+decisions_list = decision_at.get(DECISION_TABLE_NAME)
 decisions = decisions_list['records']
 while 'offset' in decisions_list.keys():
-	decisions_list = decision_at.get('Decisions', offset=decisions_list['offset'])
+	decisions_list = decision_at.get(DECISION_TABLE_NAME, offset=decisions_list['offset'])
 	decisions += decisions_list['records']
 
 reviewed_applications = set()
@@ -43,40 +92,47 @@ for decision in decisions:
 		if decision['fields']['Interview'] == 'Yes':
 			TOTAL_YES -= 1
 
-# Add auto rejected applicants to reviewed applicants
-applications_list = application_at.get('Applications')
+applications_list = application_at.get(APPLICATION_TABLE_NAME)
 applications = applications_list['records']
 while 'offset' in applications_list.keys():
-	applications_list = application_at.get('Applications', offset=applications_list['offset'])
+	applications_list = application_at.get(APPLICATION_TABLE_NAME, offset=applications_list['offset'])
 	applications += applications_list['records']
 random.shuffle(applications)
 
-for application in applications:
-	if 'Status' in application['fields'].keys() and application['fields']['Status'] == 'Auto-Rejection':
-		reviewed_applications.add(application['fields']['Name'])
+'''AUTO REJECTION LOGIC - SKIP FOR SPRING 2018'''
+# # Add auto rejected applicants to reviewed applicants
+# for application in applications:
+# 	if 'Status' in application['fields'].keys() and application['fields']['Status'] == 'Auto-Rejection':
+# 		reviewed_applications.add(application['fields']['Name'])
 
-skipped_fields = ['Email', 'Phone Number', 'Time Submitted', 'Please indicate ALL your availabilities (allocate 45 minutes before and after the time slots for travel).']
-committees = set(['Community', 'Curriculum', 'DeCal Advisor', 'Events', 'External Relations', 'Finance', 'Professional Development', 'Publicity'])
-word_count_fields = set(['Explain why the committee above is your first choice. (minimum 100 words)', 'Explain why the committee above is your second choice. (minimum 100 words)', 'Why do you want to join ANova? How do you personally resonate with ANova\'s mission statement? (minimum 300 words)', 'As a member of our club, how will you contribute to our organization? (minimum 200 words)'])
+committees = set(['Community', 'Curriculum', 'External Relations', 'GM Advisors', 'Professional Development', 'Publicity', 'Technology', 'Finance'])
+word_count_fields = set([
+						'Explain why the committee above is your first choice. (minimum 100 words)', \
+						'Explain why the committee above is your second choice. (minimum 100 words)', \
+						'Why do you want to join ANova? How do you personally resonate with ANova\'s mission statement? (minimum 300 words)', \
+						'Tell us about your most memorable teaching or mentorship experience. (max 150 words)', \
+						'What does addressing systemic issues and inequality in education look like to you? (max 150 words)'
+					])
+
 for application in applications:
 	# Printing application
 	application = application['fields']
 
 	if application['Name'] not in reviewed_applications:
+		application = reorder_dict(application)
 		for key, value in application.items():
-			if key not in skipped_fields:
-				if str(value) in committees:
-					print(color.BOLD + key + color.END + ' -- ' + color.RED + str(value).lstrip() + color.END + '\n')
-				elif str(key) in word_count_fields:
-					print(color.BOLD + key + color.END + ' -- ' + str(value).lstrip())
-					print(color.BOLD + "Word Count: " + color.END + color.DARKCYAN + str(len(value.split(" "))) + color.END + '\n')
-				else:
-					print(color.BOLD + key + color.END + ' -- ' + str(value).lstrip() + '\n')
+			if str(value) in committees:
+				print(color.BOLD + key + color.END + ' -- ' + color.RED + str(value).lstrip() + color.END + '\n')
+			elif str(key) in word_count_fields:
+				print(color.BOLD + key + color.END + ' -- ' + str(value).lstrip())
+				print(color.BOLD + "Word Count: " + color.END + color.DARKCYAN + str(len(value.split(" "))) + color.END + '\n')
+			else:
+				print(color.BOLD + key + color.END + ' -- ' + str(value).lstrip() + '\n')
 
 # 		# Prompting for decision
 		decision = ''
 		while decision != 'y' and decision != 'n':
-			decision = str(input('You have ' + color.BLUE + str(len(applications) - len(reviewed_applications)) + color.END + ' applications left with ' + color.BLUE + str(TOTAL_YES) + color.END + ' more applicants you can accept. Do you want to give this applicant an interview? (y/n) '))
+			decision = str(input('You have ' + color.BLUE + str(len(applications) - len(reviewed_applications)) + color.END + ' applications left with ' + color.BLUE + str(TOTAL_YES) + color.END + ' more applicants you can accept. Do you want to give this applicant an interview? (y/n) ')).lower()
 
 		reviewed_applications.add(application['Name'])
 
